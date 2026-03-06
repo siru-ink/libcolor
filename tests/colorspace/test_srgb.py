83ed9b7c46cd1b01b328d7f6e83939bfe2fa6bef
siru <email@siru.ink>
2026-03-06 20:47:12 +0100

    Added tests for hex colorcode constructor from SRGBColor class

--- tests/colorspace/test_srgb.py
+++ tests/colorspace/test_srgb.py
@@ -15,3 +15,8 @@ class TestSRGBColor:
     @pytest.fixture
     def obj3(self) -> srgb.SRGBColor:
         return srgb.SRGBColor(143, 188, 143)  # darkseagreen
+
+    def test_from_hex_str(self, obj1, obj2, obj3) -> None:
+        assert srgb.SRGBColor.from_hex_str("000") == obj1
+        assert srgb.SRGBColor.from_hex_str("FFFFFF") == obj2
+        assert srgb.SRGBColor.from_hex_str("#8FBC8F") == obj3
