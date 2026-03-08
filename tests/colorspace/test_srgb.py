f0605b26ddcb3abb6648d0200935359c7260a356
siru <email@siru.ink>
2026-03-08 15:40:53 +0100

    Added pytests for linearized RGB color from (standard) color

--- tests/colorspace/test_srgb.py
+++ tests/colorspace/test_srgb.py
@@ -40,3 +40,50 @@ class TestStandardRGBColor:
         assert not obj1 >= obj2
         assert obj2 >= obj1
         assert obj2 >= obj2
+
+
+class TestStandardRGBLiearizedColor:
+    @pytest.fixture
+    def obj1(self) -> srgb.StandardRGBLinearizedColor:
+        return srgb.StandardRGBLinearizedColor(0.0, 0.0, 0.0)
+
+    @pytest.fixture
+    def obj2(self) -> srgb.StandardRGBLinearizedColor:
+        return srgb.StandardRGBLinearizedColor(1.0, 1.0, 1.0)
+
+    @pytest.fixture
+    def obj3(self) -> srgb.StandardRGBLinearizedColor:
+        return srgb.StandardRGBLinearizedColor(
+            0.27467731206038465, 0.5028864580325687, 0.27467731206038465
+        )
+
+    @pytest.fixture
+    def non_linearized_obj1(self) -> srgb.StandardRGBColor:
+        return srgb.StandardRGBColor(0, 0, 0)  # black
+
+    @pytest.fixture
+    def non_linearized_obj2(self) -> srgb.StandardRGBColor:
+        return srgb.StandardRGBColor(255, 255, 255)  # white
+
+    @pytest.fixture
+    def non_linearized_obj3(self) -> srgb.StandardRGBColor:
+        return srgb.StandardRGBColor(143, 188, 143)  # darkseagreen
+
+    def test_from_standard_rgb_color(
+        self,
+        obj1: srgb.StandardRGBLinearizedColor,
+        obj2: srgb.StandardRGBLinearizedColor,
+        obj3: srgb.StandardRGBLinearizedColor,
+        non_linearized_obj1: srgb.StandardRGBColor,
+        non_linearized_obj2: srgb.StandardRGBColor,
+        non_linearized_obj3: srgb.StandardRGBColor,
+    ) -> None:
+        assert obj1 == srgb.StandardRGBLinearizedColor.from_standard_rgb_color(
+            non_linearized_obj1
+        )
+        assert obj2 == srgb.StandardRGBLinearizedColor.from_standard_rgb_color(
+            non_linearized_obj2
+        )
+        assert obj3 == srgb.StandardRGBLinearizedColor.from_standard_rgb_color(
+            non_linearized_obj3
+        )
